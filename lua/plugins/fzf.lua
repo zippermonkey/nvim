ac7f70f13aa5518ca84d44c0fb3c095fd2396f47
zippermonkey <1960727314@qq.com>
2024-07-22 18:56:02 +0800

    fzf: add keybinding to list buffers

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -7,6 +7,7 @@ return {
     { '<leader>ff', '<cmd>FzfLua files<cr>', mode = 'n', desc = '[f]ind [f]iles' },
     { '<leader>rr', '<cmd>FzfLua resume<cr>', mode = 'n', desc = '[r]esume sea[r]ch' },
     { '<leader>fb', '<cmd>FzfLua buffers<cr>', mode = 'n', desc = '[f]ind [b]uffers' },
+    { '<leader>bs', '<cmd>FzfLua buffers<cr>', mode = 'n', desc = '[f]ind [b]uffers' },
     { '<leader>fo', '<cmd>FzfLua oldfiles<cr>', mode = 'n', desc = '[f]ind [o]ldfiles' },
     { '<leader>fg', '<cmd>FzfLua live_grep<cr>', mode = 'n', desc = '[f]ind somethinf with live [g]rep' },
     { '<leader>fw', '<cmd>FzfLua grep_cword<cr>', mode = 'n', desc = '[f]ind [w]ord' },
