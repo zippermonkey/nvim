93f490fb365dde355b664181589cae3f904d9ec3
zippermonkey <1960727314@qq.com>
2024-07-22 18:50:07 +0800

    colorscheme: tokyonigh

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,9 @@
 return {
-  'Mofiqul/vscode.nvim',
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
-    require('vscode').setup {
-      -- Enable italic comment
-      italic_comments = true,
-      -- Underline `@markup.link.*` variants
-      underline_links = true,
-      -- Disable nvim-tree background color
-      disable_nvimtree_bg = true,
-    }
-    vim.cmd.colorscheme 'vscode'
-  end,
+    vim.cmd.colorscheme 'tokyonight-night'
+  end
 }
