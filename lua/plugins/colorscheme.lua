c4061831c08fe2ea955a205a299d4c464b115251
zippermonkey <1960727314@qq.com>
2025-09-08 09:39:21 +0800

    change colorcheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,17 @@
+-- return {
+--   -- "folke/tokyonight.nvim",
+--   'nyoom-engineering/oxocarbon.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   opts = {},
+--   config = function()
+--     vim.cmd [[colorscheme oxocarbon]]
+--   end,
+-- }
 return {
-  -- "folke/tokyonight.nvim",
-  'nyoom-engineering/oxocarbon.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {},
+  'rose-pine/neovim',
+  name = 'rose-pine',
   config = function()
-    vim.cmd [[colorscheme oxocarbon]]
+    vim.cmd 'colorscheme rose-pine'
   end,
 }
