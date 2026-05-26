becea42686e7b6856b03aa21e2a028604f24cdc8
zippermonkey <1960727314@qq.com>
2026-05-26 20:49:18 +0800

    treesitter: rename 'nvim-treesitter.configs' for 'nvim-treesitter.config' (without s)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   config = function()
-    require('nvim-treesitter.configs').setup {
+    require('nvim-treesitter.config').setup {
       ensure_installed = { 'html', 'css', 'vim', 'lua', 'javascript', 'typescript', 'tsx', 'c', 'rust', 'python' },
       auto_install = true,
       highlight = {
