ff060f4cda9191030a360402bce28e89090da895
zippermonkey <1960727314@qq.com>
2026-05-27 16:09:05 +0800

    auto format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -17,23 +17,11 @@ return { -- PLUGIN: Autoformat
   },
   opts = {
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = {
-        c = true,
-        cpp = true,
-      }
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        return nil
-      else
-        return {
-          timeout_ms = 500,
-          lsp_format = 'fallback',
-        }
-      end
-    end,
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
