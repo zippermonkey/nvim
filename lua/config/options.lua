c3b65e311d82993cf11f0176f1ee95a38cc71309
bruzzhang <bruzzhang@tencent.com>
2026-06-17 16:42:11 +0800

    disable relative line number

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.smartindent = true
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
